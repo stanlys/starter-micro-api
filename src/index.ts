acd1e910f0732e850f0e2e60a1e7401a7c162812
Stanlys <stanlys@yandex.ru>
2022-11-28 12:34:41 +0300

    feat: connect final-task server

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { PORT } from './constants';
+
+import * as serverService from './services/server.service';
+
+
+(async () => {
+  try {
+    await mongoose.connect('mongodb+srv://rsexample2022q3:7Jdi33MnTjhJgGVl@cluster0.5mql4r4.mongodb.net/pma?retryWrites=true&w=majority');
+    serverService.server.listen(process.env.PORT || PORT, function () {
+      console.log('Сервер ожидает подключения...');
+    })
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+
+
+process.on('SIGINT', async () => {
+  await mongoose.disconnect();
+  process.exit();
+});
---
+++ src/models/board.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+const boardScheme = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    users: {
+      type: [String],
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
+
+export default mongoose.model("Board", boardScheme);
